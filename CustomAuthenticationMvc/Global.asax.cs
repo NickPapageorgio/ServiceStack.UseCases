28918461512e8883a59f4e04f397ca644b0cce99
NickPapageorgio <mkrtichie@gmail.com>
2014-06-05 17:22:02 -0400

    Revert "Tweak use-cases"
    This reverts commit c143991dba3c821e04ded125058f03af03976b35.

--- CustomAuthenticationMvc/Global.asax.cs
+++ CustomAuthenticationMvc/Global.asax.cs
@@ -1,8 +1,13 @@
-﻿using System.Web.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using CustomAuthenticationMvc.App_Start;
+using CustomAuthenticationMvc.Helper;
 
 namespace CustomAuthenticationMvc
 {
--- HelloWorld/Global.asax.cs
+++ HelloWorld/Global.asax.cs
@@ -35,9 +35,6 @@ namespace HelloWorld
         public string Name { get; set; }
     }
 
-    [Route("/hellofile")]
-    public class HelloFile { }
-
     [Route("/helloimage/{Name}")]
     public class HelloImage
     {
@@ -64,20 +61,9 @@ namespace HelloWorld
     //Implementation
     public class HelloService : Service
     {
-        [AddHeader(ContentType = MimeTypes.Html)]
         public object Get(HelloHtml request)
         {
-            return "<h1>Hello, HTML!</h1>"
-               + "<div><a href='{0}'>Hello File</a></div>".Fmt(new HelloFile().ToGetUrl())
-               + "<div><a href='{0}'>Hello Image</a></div>".Fmt(new HelloImage
-               {
-                   Name = "World",
-                   Background = "Blue",
-                   Foreground = "Yellow",
-                   FontSize = 48,
-                   Height = 600,
-                   Width = 800,
-               }.ToGetUrl());
+            return "<h1>Hello, {0}!</h1>".Fmt(request.Name);
         }
 
         [AddHeader(ContentType = "text/plain")]
@@ -86,12 +72,7 @@ namespace HelloWorld
             return "<h1>Hello, {0}!</h1>".Fmt(request.Name);
         }
 
-        public object Get(HelloFile request)
-        {
-            byte[] imageBytes = "https://www.google.com/images/srpr/logo11w.png".GetBytesFromUrl();
-            return new HttpResult(imageBytes, "image/png");
-        }
-
+        [AddHeader(ContentType = "image/png")]
         public object Get(HelloImage request)
         {
             var width = request.Width.GetValueOrDefault(640);
@@ -109,15 +90,12 @@ namespace HelloWorld
                 var drawBrush = new SolidBrush(fgColor);
                 var drawRect = new RectangleF(0, 0, width, height);
 
-                var drawFormat = new StringFormat
-                {
+                var drawFormat = new StringFormat {
                     LineAlignment = StringAlignment.Center,
-                    Alignment = StringAlignment.Center
-                };
+                    Alignment = StringAlignment.Center };
 
                 g.DrawString(drawString, drawFont, drawBrush, drawRect, drawFormat);
 
-                base.Response.ContentType = "image/png";
                 var ms = new MemoryStream();
                 image.Save(ms, ImageFormat.Png);
                 return ms;
